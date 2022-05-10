fd2ec1811b3ecfe69e2142d0e1241f11dfeeb884
tedo0627 <24734045+tedo0627@users.noreply.github.com>
2022-05-10 16:44:51 +0900

    キーの登録場所を変更

--- forge/src/main/java/mcpc/tedo0627/kzeaddon/forge/KZEAddon.java
+++ forge/src/main/java/mcpc/tedo0627/kzeaddon/forge/KZEAddon.java
@@ -5,10 +5,8 @@ import mcpc.tedo0627.kzeaddon.forge.service.HidePlayerService;
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmlclient.registry.ClientRegistry;
 
 @Mod(KZEAddon.MOD_ID)
 public class KZEAddon {
@@ -18,7 +16,6 @@ public class KZEAddon {
     private CustomConfig config;
 
     private final KeyMapping settingKey = new KeyMapping("Open KZEAddon settings key", InputConstants.Type.KEYSYM, -1, "KZEAddon");
-    private final KeyMapping hidePlayerKey = new KeyMapping("Hide player toggle key", InputConstants.Type.KEYSYM, -1, "KZEAddon");
 
     public KZEAddon() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::complete);
@@ -26,8 +23,7 @@ public class KZEAddon {
 
     private void complete(FMLLoadCompleteEvent event) {
         //config = new CustomConfig();
-        MinecraftForge.EVENT_BUS.register(new HidePlayerService(hidePlayerKey));
+        MinecraftForge.EVENT_BUS.register(new HidePlayerService());
         //ClientRegistry.registerKeyBinding(settingKey);
-        ClientRegistry.registerKeyBinding(hidePlayerKey);
     }
 }
--- forge/src/main/java/mcpc/tedo0627/kzeaddon/forge/service/HidePlayerService.java
+++ forge/src/main/java/mcpc/tedo0627/kzeaddon/forge/service/HidePlayerService.java
@@ -1,11 +1,13 @@
 package mcpc.tedo0627.kzeaddon.forge.service;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.scores.Team;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fmlclient.registry.ClientRegistry;
 
 import java.util.UUID;
 
@@ -13,25 +15,26 @@ public class HidePlayerService {
 
     private static boolean executing = false;
 
-    public static boolean isInvisible(UUID uuid, Team team2, boolean bool) {
-        if (team2 == null) return bool;
+    public static boolean isInvisible(UUID uuid, Team team, boolean bool) {
+        if (team == null) return bool;
 
         Minecraft mc = Minecraft.getInstance();
         Player player = mc.player;
         if (player == null) return bool;
+
         if (player.getUUID().equals(uuid)) return bool;
 
         Team team1 = player.getTeam();
         if (team1 == null) return bool;
 
-        if (!team1.getName().equals(team2.getName())) return bool;
+        if (!team1.getName().equals(team.getName())) return bool;
         return executing || bool;
     }
 
-    private final KeyMapping key;
+    private final KeyMapping key = new KeyMapping("Hide player toggle key", InputConstants.Type.KEYSYM, -1, "KZEAddon");
 
-    public HidePlayerService(KeyMapping key) {
-        this.key = key;
+    public HidePlayerService() {
+        ClientRegistry.registerKeyBinding(key);
     }
 
     @SubscribeEvent

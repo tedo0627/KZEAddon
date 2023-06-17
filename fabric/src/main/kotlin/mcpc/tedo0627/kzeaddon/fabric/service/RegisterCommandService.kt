25d048939513b53fd3c541c8b8bc33d098d8a2ce
tedo0627 <24734045+tedo0627@users.noreply.github.com>
2023-06-17 23:13:42 +0900

    [Fabric] サーバー内でコマンドが使えなかったのを修正

--- fabric/src/main/kotlin/mcpc/tedo0627/kzeaddon/fabric/service/RegisterCommandService.kt
+++ fabric/src/main/kotlin/mcpc/tedo0627/kzeaddon/fabric/service/RegisterCommandService.kt
@@ -3,20 +3,30 @@ package mcpc.tedo0627.kzeaddon.fabric.service
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import mcpc.tedo0627.kzeaddon.fabric.screen.SettingScreen
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.minecraft.client.MinecraftClient
-import net.minecraft.server.command.ServerCommandSource
 
 class RegisterCommandService {
 
+    private var enable = false
+
     init {
-        CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
-            dispatcher.register(LiteralArgumentBuilder.literal<ServerCommandSource?>("kzeaddon")
+        ClientCommandRegistrationCallback.EVENT.register { dispatcher, _ ->
+            dispatcher.register(LiteralArgumentBuilder.literal<FabricClientCommandSource>("kzeaddon")
                 .executes {
-                    MinecraftClient.getInstance().setScreen(SettingScreen())
+                    enable = true
                     Command.SINGLE_SUCCESS
                 }
             )
+        }
+
+        ClientTickEvents.END_CLIENT_TICK.register {
+            if (enable) {
+                MinecraftClient.getInstance().setScreen(SettingScreen())
+                enable = false
+            }
         }
     }
 }

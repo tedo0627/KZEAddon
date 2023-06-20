acf05124f0cbabc186774feaf25f2624a5f44f76
tedo0627 <24734045+tedo0627@users.noreply.github.com>
2023-06-21 02:16:17 +0900

    hidePlayerのenumの翻訳キーを変更

--- fabric/src/main/kotlin/mcpc/tedo0627/kzeaddon/fabric/option/HideToggleType.kt
+++ fabric/src/main/kotlin/mcpc/tedo0627/kzeaddon/fabric/option/HideToggleType.kt
@@ -3,8 +3,8 @@ package mcpc.tedo0627.kzeaddon.fabric.option
 import net.minecraft.util.TranslatableOption
 
 enum class HideToggleType(private val id: Int, private val key: String) : TranslatableOption {
-    SWITCH(0, "kzeaddon.options.enum.switch"),
-    LONG_PRESS(1, "kzeaddon.options.enum.long_press");
+    SWITCH(0, "kzeaddon.options.hideToggleType.switch"),
+    LONG_PRESS(1, "kzeaddon.options.hideToggleType.long_press");
 
     override fun getId() = id
 

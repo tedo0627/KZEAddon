325731c37081272676cd057faf4b57df859d2f41
tedo0627 <24734045+tedo0627@users.noreply.github.com>
2023-06-23 01:11:54 +0900

    [Forge] importの最適化

--- forge/src/main/kotlin/mcpc/tedo0627/kzeaddon/forge/service/KillLogService.kt
+++ forge/src/main/kotlin/mcpc/tedo0627/kzeaddon/forge/service/KillLogService.kt
@@ -5,7 +5,6 @@ import com.mojang.blaze3d.systems.RenderSystem
 import com.mojang.blaze3d.vertex.PoseStack
 import mcpc.tedo0627.kzeaddon.forge.option.AddonOptions
 import net.minecraft.client.Minecraft
-import net.minecraft.client.gui.Gui
 import net.minecraft.client.gui.GuiComponent
 import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation

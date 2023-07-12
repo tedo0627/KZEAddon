e6eb932562892559e018f9c6b44bd7e1a53ae9ce
tedo0627 <24734045+tedo0627@users.noreply.github.com>
2023-07-12 22:20:19 +0900

    Overlayの座標を変えるScreenをいい感じにした

--- fabric/src/main/kotlin/mcpc/tedo0627/kzeaddon/fabric/screen/OverlayLocationScreen.kt
+++ fabric/src/main/kotlin/mcpc/tedo0627/kzeaddon/fabric/screen/OverlayLocationScreen.kt
@@ -1,12 +1,7 @@
 package mcpc.tedo0627.kzeaddon.fabric.screen
 
 import com.mojang.blaze3d.vertex.PoseStack
-import mcpc.tedo0627.kzeaddon.fabric.option.AddonOptions
-import net.minecraft.client.OptionInstance
 import net.minecraft.client.gui.components.Button
-import net.minecraft.client.gui.components.EditBox
-import net.minecraft.client.gui.components.StringWidget
-import net.minecraft.client.gui.components.Tooltip
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.network.chat.CommonComponents
 import net.minecraft.network.chat.Component
@@ -14,17 +9,7 @@ import net.minecraft.network.chat.Component
 class OverlayLocationScreen(private val previous: Screen? = null) : Screen(Component.literal("座標の変更")) {
 
     override fun init() {
-        add("currentBulletX", "tooltipX", AddonOptions.currentBulletOverlayLocationX)
-        add("currentBulletY", "tooltipY", AddonOptions.currentBulletOverlayLocationY)
-
-        add("remainingBulletX", "tooltipX", AddonOptions.remainingBulletOverlayLocationX)
-        add("remainingBulletY", "tooltipY", AddonOptions.remainingBulletOverlayLocationY)
-
-        add("killLogX", "tooltipX", AddonOptions.killLogOverlayLocationX)
-        add("killLogY", "tooltipY", AddonOptions.killLogOverlayLocationY)
-
-        add("glassTimerX", "tooltipX", AddonOptions.glassTimerOverlayLocationX)
-        add("glassTimerY", "tooltipY", AddonOptions.glassTimerOverlayLocationY)
+        addRenderableWidget(OverlayLocationList(width, height, 32, height - 32, 25))
 
         addRenderableWidget(Button
             .Builder(CommonComponents.GUI_DONE) { minecraft?.setScreen(previous) }
@@ -32,29 +17,6 @@ class OverlayLocationScreen(private val previous: Screen? = null) : Screen(Compo
             .size(200, 20)
             .build()
         )
-    }
-
-    private fun add(translatable: String, toolTipStr: String, option: OptionInstance<Int>) {
-        val size = children().size / 2
-        val y = height / 6 - 12 + size * 30
-
-        val textRenderer = minecraft?.font ?: return
-        val toolTip = Tooltip.create(Component.translatable("kzeaddon.screen.overlayLocation.$toolTipStr"))
-        val text = Component.translatable("kzeaddon.screen.overlayLocation.$translatable")
-
-        val textWidget = StringWidget(width / 2 - 250 + textRenderer.width(text.visualOrderText) / 2, y, 140, 20, text, textRenderer)
-        textWidget.setTooltip(toolTip)
-        addRenderableWidget(textWidget)
-
-        val textFieldWidget = EditBox(textRenderer, width / 2 + 40, y, 80, 20, text)
-        textFieldWidget.value = option.get().toString()
-        textFieldWidget.setResponder {
-            val value = it.toIntOrNull()
-            textFieldWidget.setTextColor(if (value != null) 0xE0E0E0 else 0xFF0000)
-            if (value != null) option.set(value)
-        }
-        textFieldWidget.setTooltip(toolTip)
-        addRenderableWidget(textFieldWidget)
     }
 
     override fun render(poseStack: PoseStack, i: Int, j: Int, f: Float) {
---
+++ forge/src/main/kotlin/mcpc/tedo0627/kzeaddon/forge/screen/OverlayLocationList.kt
@@ -0,0 +1,82 @@
+package mcpc.tedo0627.kzeaddon.forge.screen
+
+import com.mojang.blaze3d.vertex.PoseStack
+import mcpc.tedo0627.kzeaddon.forge.option.AddonOptions
+import net.minecraft.client.Minecraft
+import net.minecraft.client.OptionInstance
+import net.minecraft.client.gui.components.*
+import net.minecraft.network.chat.Component
+
+class OverlayLocationList(
+    width: Int, height: Int, y0: Int, y1: Int, itemHeight: Int
+) : ContainerObjectSelectionList<OverlayLocationList.OverlayLocationEntry>(Minecraft.getInstance(), width, height, y0, y1, itemHeight) {
+
+    init {
+        add("currentBullet", AddonOptions.currentBulletOverlayLocationX, AddonOptions.currentBulletOverlayLocationY)
+        add("remainingBullet", AddonOptions.remainingBulletOverlayLocationX, AddonOptions.remainingBulletOverlayLocationY)
+        add("killLog", AddonOptions.killLogOverlayLocationX, AddonOptions.killLogOverlayLocationY)
+        add("glassTimer", AddonOptions.glassTimerOverlayLocationX, AddonOptions.glassTimerOverlayLocationY)
+
+        setRenderBackground(false)
+        setRenderTopAndBottom(false)
+    }
+
+    private fun add(translatable: String, optionX: OptionInstance<Int>, optionY: OptionInstance<Int>) {
+        addEntry(OverlayLocationEntry(width, translatable, optionX, optionY))
+    }
+
+    override fun getRowWidth() = 400
+
+    override fun getScrollbarPosition() = super.getScrollbarPosition() + 32
+
+    class OverlayLocationEntry(
+        width: Int, translatable: String,
+        optionX: OptionInstance<Int>, optionY: OptionInstance<Int>
+    ) : Entry<OverlayLocationEntry>() {
+
+        private val children = mutableListOf<AbstractWidget>()
+
+        init {
+            val font = Minecraft.getInstance().font
+            val text = Component.translatable("kzeaddon.screen.overlayLocation.$translatable")
+
+            val textWidget = StringWidget(width / 2 - 250 + font.width(text.visualOrderText) / 2, 0, 140, 20, text, font)
+            children.add(textWidget)
+
+            val textFieldWidgetX = EditBox(font, width / 2 + 40, 0, 80, 20, text)
+            textFieldWidgetX.value = optionX.get().toString()
+            textFieldWidgetX.setResponder {
+                val value = it.toIntOrNull()
+                textFieldWidgetX.setTextColor(if (value != null) 0xE0E0E0 else 0xFF0000)
+                if (value != null) optionX.set(value)
+            }
+            val toolTipX = Tooltip.create(Component.translatable("kzeaddon.screen.overlayLocation.tooltipX"))
+            textFieldWidgetX.setTooltip(toolTipX)
+            children.add(textFieldWidgetX)
+
+            val textFieldWidgetY = EditBox(font, width / 2 + 130, 0, 80, 20, text)
+            textFieldWidgetY.value = optionY.get().toString()
+            textFieldWidgetY.setResponder {
+                val value = it.toIntOrNull()
+                textFieldWidgetY.setTextColor(if (value != null) 0xE0E0E0 else 0xFF0000)
+                if (value != null) optionY.set(value)
+            }
+            val toolTipY = Tooltip.create(Component.translatable("kzeaddon.screen.overlayLocation.tooltipY"))
+            textFieldWidgetY.setTooltip(toolTipY)
+            children.add(textFieldWidgetY)
+        }
+
+        override fun render(
+            poseStack: PoseStack, i: Int, j: Int, k: Int, l: Int, m: Int, n: Int, o: Int, bl: Boolean, f: Float
+        ) {
+            children.forEach { abstractWidget: AbstractWidget ->
+                abstractWidget.y = j
+                abstractWidget.render(poseStack, n, o, f)
+            }
+        }
+
+        override fun children() = children
+
+        override fun narratables() = children
+    }
+}
